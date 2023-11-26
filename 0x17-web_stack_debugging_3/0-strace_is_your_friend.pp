367e032ef74eff668b6c86286733610e46a7746e
shield <myshield02@gmail.com>
2023-11-26 22:50:21 +0100

    deleted directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,8 +0,0 @@
-# Fixing your Apache sever
-# Apache server
-
-exec { 'fix apache config':
-  command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-}
-
