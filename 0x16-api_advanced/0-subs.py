ad6ee2dc67a00bcd2a54b7d0856a64654682971d
Otuekong Etim <myshield02@gmail.com>
2024-01-16 14:05:43 +0100

    total reddit subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Query the REDDIT API and find the
+total number of subscribers
+"""
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+    Returns the number of subscribers of reddit
+    """
+    url = f"https://api.reddit.com/r/{subreddit}/about"
+    headers = {'User-Agent': 'CustomUserAgen/1.1'}
+    res = requests.get(url=url, headers=headers, allow_redirects=False)
+
+    if res.status_code != 200:
+        return (0)
+    res = res.json()
+    if 'data' in res:
+        return res.get('data').get('subscribers')
+    else:
+        return (0)
