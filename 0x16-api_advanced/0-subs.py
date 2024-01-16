3b3b74bee9b8a0218ee286a6a6d83f7ab01ef5b1
Otuekong Etim <myshield02@gmail.com>
2024-01-16 14:10:53 +0100

    total reddit subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,14 +9,14 @@ def number_of_subscribers(subreddit):
     """
     Returns the number of subscribers of reddit
     """
-    url = (f"https://api.reddit.com/r/{subreddit}/about")
-    headers = {'User-Agent': 'CustomUserAgen/1.1'}
+    url = f"https://api.reddit.com/r/{subreddit}/about"
+    headers = {'User-Agent': 'Google Chrome Version 81.0.4044.129'}
     res = requests.get(url=url, headers=headers, allow_redirects=False)
 
     if res.status_code != 200:
-        return (0)
+        return 0
     res = res.json()
     if 'data' in res:
         return res.get('data').get('subscribers')
     else:
-        return (0)
+        return 0
