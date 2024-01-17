59115010aa7d5d6ff3193872f94783cc080fce63
Otuekong Etim <myshield02@gmail.com>
2024-01-17 13:53:10 +0100

    automate error debugging with puppet

--- 0x17-web_stack_debugging_3/0-strace_is-your_friend.pp
+++
@@ -1,6 +0,0 @@
-# Find out why Apache is returning 500 error, then automate with Puppet
-
-exec { 'fix_apache_500':
-  command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
-  path    => ['/bin','/usr/bin'],
-}
