4d4acd35b3824dfdddba8a5962075c277d775415
Otuekong Etim <myshield02@gmail.com>
2024-01-18 12:01:09 +0100

    added params argument

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """
-Queries the Reddit API and prints the titles of the 
+Queries the Reddit API and prints the titles of the
 first 10 hot posts listed for a given subreddit
 """
 import requests
+
 
 def top_ten(subreddit):
     """
@@ -11,14 +12,16 @@ def top_ten(subreddit):
     """
     url = f"https://api.reddit.com/r/{subreddit}?sort=hot&limit=10"
     headers = {'User-Agent': 'Google Chrome Version 81.0.4044.129'}
-    res = requests.get(url=url, headers=headers, allow_redirects=False)
+    params = {'limit': 10}
+    res = requests.get(url=url, headers=headers, params=params, allow_redirects=False)
 
     if res.status_code != 200:
         print(None)
         return
-    res = res.json()
-    if 'data' in res:
-        for posts in res.get('data').get('children'):
+    result = res.json()
+    try:
+        data = result.get('data').get('children')
+        for posts in data:
             print(posts.get('data').get('title'))
-    else:
+    except Exception:
         print(None)
