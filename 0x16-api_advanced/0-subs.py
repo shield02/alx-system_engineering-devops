50213ff19e2840c4bd62a549b3c0529a1ac8493d
Otuekong Etim <myshield02@gmail.com>
2024-01-18 11:03:16 +0100

    check instance of subreddit and use try except block

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,11 +10,10 @@ def number_of_subscribers(subreddit):
     """
     Returns the number of subscribers of reddit
     """
-
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    url = f"https://api.reddit.com/r/{subreddit}/about"
+    url = "https://api.reddit.com/r/{}/about".format(subreddit)
     headers = {'User-Agent': 'Google Chrome Version 81.0.4044.129'}
     res = requests.get(url, headers=headers)
     result = res.json()
